f9d88259414445d4dd5cc151fde49f1260868b4d
austinbra <129190342+austinbra@users.noreply.github.com>
2024-01-04 02:02:01 -0600

    temp
    faster now but still double clicking

--- main.py
+++ main.py
@@ -2,40 +2,50 @@ import pyautogui
 import time
 
 target = (149, 195, 232)
+target2 = (255, 255, 255)
+ops = (43, 135, 209)
 screenshot = None
+count = 0
+
 
 def checkColor(x, y):
     global screenshot
     if screenshot is None:
         screenshot = pyautogui.screenshot()
     pixel_color = screenshot.getpixel((x, y))
-
-    if pixel_color == target:
-        pyautogui.click(x, y)
+    if pixel_color != ops:
         screenshot = None  # Reset the screenshot after click
         return True
     return False
+    #print(pixel_color)
+
 
 def grid(rows, cols, col_step_size, row_step_size):
-    screen_width, screen_height = pyautogui.size()
-    for row in range(6, rows + 3):
-        for col in range(5, cols - 8):
+    for row in range(5, rows + 4):
+        for col in range(6, cols - 9):
             x = col * col_step_size
             y = row * row_step_size
-            checkColor(int(x), int(y))
-            pyautogui.moveTo(x, y)
+            if checkColor(int(x), int(y)):
+                pyautogui.moveTo(x, y)
+                pyautogui.click(x, y)
+                return True
+    return False
 
 pyautogui.PAUSE = 0.0
 pyautogui.MINIMUM_DURATION = 0.0
 
 # Define grid parameters
+screen_width, screen_height = pyautogui.size()
 grid_rows = 10
-grid_cols = 30
+grid_cols = 35
 col_step_size = 100  # Adjust this based on your preference
 row_step_size = 100
 
 # Call the function to move the mouse in a grid pattern
 start = time.time()
-end = start + 15
+end = start + 5
+pyautogui.click(screen_width/2, (screen_height*3)/7)
 while time.time() < end:
     grid(grid_rows, grid_cols, col_step_size, row_step_size)
+    #count += 1
+
