acecbaaf8d19da6951e0a054753719b6f00cad53
austinbra <129190342+austinbra@users.noreply.github.com>
2024-01-03 23:50:35 -0600

    faster

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
-
 import pyautogui
 import time
 
+target = (149, 195, 232)
+screenshot = None
+
 def checkColor(x, y):
-    #finds the rgb values at the coordinate
-    pixel_color = pyautogui.pixel(x, y)
-    if pixel_color == (149,195,232):
-        pyautogui.click(x,y)
+    global screenshot
+    if screenshot is None:
+        screenshot = pyautogui.screenshot()
+    pixel_color = screenshot.getpixel((x, y))
+
+    if pixel_color == target:
+        pyautogui.click(x, y)
+        screenshot = None  # Reset the screenshot after click
         return True
     return False
 
 def grid(rows, cols, col_step_size, row_step_size):
     screen_width, screen_height = pyautogui.size()
-    for row in range(4, rows - 4):
-        for col in range(1, cols - 1):
+    for row in range(6, rows + 3):
+        for col in range(5, cols - 8):
             x = col * col_step_size
             y = row * row_step_size
-            checkColor(int(x),int(y))
+            checkColor(int(x), int(y))
             pyautogui.moveTo(x, y)
 
 pyautogui.PAUSE = 0.0
 pyautogui.MINIMUM_DURATION = 0.0
+
 # Define grid parameters
-grid_rows = 20
-grid_cols = 25
-col_step_size = float(pyautogui.size()[0]) / grid_cols  # Adjust this based on your preference
-row_step_size = float(pyautogui.size()[1]) / grid_cols
+grid_rows = 10
+grid_cols = 30
+col_step_size = 100  # Adjust this based on your preference
+row_step_size = 100
+
 # Call the function to move the mouse in a grid pattern
 start = time.time()
-end = start + 30
+end = start + 15
 while time.time() < end:
     grid(grid_rows, grid_cols, col_step_size, row_step_size)
-
-
